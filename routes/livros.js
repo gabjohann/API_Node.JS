7898b5d1c35b2a01e196c636da32766c8658a3a3
Lucas Gabriel Fernandes Johann <lukito@pop-os.localdomain>
2021-09-03 15:30:25 -0300

    database full connected

--- routes/livros.js
+++ routes/livros.js
@@ -2,25 +2,34 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 
+//  RETORNA TODOS OS LIVROS
 router.get('/', (req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Retorna todos os livros.',
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: erro });
+    }
+    conn.query('SELECT * FROM livros;', (error, resultado, fields) => {
+      if (error) {
+        return res.status(500).send({ error: error });
+      }
+      return res.status(200).send({ response: resultado });
+    });
   });
 });
 
+// INSERE UM LIVRO
 router.post('/', (req, res, next) => {
   mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
     conn.query(
       'INSERT INTO livros (titulo, ano) VALUES (?,?)',
       [req.body.titulo, req.body.ano],
       (error, resultado, field) => {
         conn.release();
-
         if (error) {
-          return res.status(500).send({
-            error: error,
-            response: null,
-          });
+          return res.status(500).send({ error: error });
         }
         res.status(201).send({
           mensagem: 'Livro inserido.',
@@ -31,15 +40,64 @@ router.post('/', (req, res, next) => {
   });
 });
 
-router.patch('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Estado do livro alterado.',
+// RETORNA OS DADOS DE UM LIVRO
+router.get('/:id_livro', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      'SELECT * FROM livros WHERE id_livro = ?;',
+      [req.params.id_livro],
+      (error, resultado, field) => {
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+        return res.status(200).send({ response: resultado });
+      }
+    );
   });
 });
 
+// ALTERA UM LIVRO
+router.patch('/', (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      'UPDATE livros SET titulo = ?, ano = ? WHERE id_livro = ?;',
+      [req.body.titulo, req.body.ano, req.body.id_livro],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+        return res.status(202).send({ mensagem: 'Livro alterado.' });
+      }
+    );
+  });
+});
+
+// EXCLUI UM LIVRO
 router.delete('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Livro deletado.',
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
+    conn.query(
+      'DELETE FROM livros WHERE id_livro = ?;',
+      [req.body.id_livro],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+        res.status(202).send({
+          mensagem: 'Livro deletado.',
+        });
+      }
+    );
   });
 });
 
