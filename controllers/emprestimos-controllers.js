aba5f41e0ad5828bf36dd03ea1eaa5f3fdc8a024
Lucas Gabriel Fernandes Johann <lukito@pop-os.localdomain>
2021-09-05 16:21:45 -0300

    api pronta

--- controllers/emprestimos-controllers.js
+++ controllers/emprestimos-controllers.js
@@ -1,27 +1,32 @@
 const mysql = require('../mysql').pool;
 
-exports.getEmprestimos =  (req, res, next) => { 
+exports.getEmprestimos = (req, res, next) => {
   mysql.getConnection((error, conn) => {
-    if (error) { return res.status(500).send({ error: error }) }
+    if (error) {
+      return res.status(500).send({ error: error });
+    }
     conn.query(
-      `SELECT emprestimos.id_emprestimo,
+      `SELECT emprestimos.id_emprestimo, 
                                         emprestimos.id_associado,
-                                        associados.nome,
-                                        associados.sobrenome,
-                                        livros.id_livro,
-                                        livros.titulo,
-                                        editoras.id_editora,
-                                        editoras.nome,
-                                        emprestimos.prazo_devolucao,
-                                        emprestimos.data,
+                                        associados.nome, 
+                                        associados.sobrenome, 
+                                        emprestimos.id_livro,
+                                        livros.titulo, emprestimos.id_editora, 
+                                        editoras.nome_editora,
+                                        emprestimos.prazo_devolucao, 
+                                        emprestimos.data_emprestimo,
                                         emprestimos.data_devolucao
-                            FROM emprestimos
-                  INNER JOIN livros
-                                  ON livros.id_livro = emprestimos.id_livro
-                 INNER JOIN associados ON associados.id_associado = emprestimos.id_associado
-                 INNER JOIN editoras ON editoras.id_editora = livros.id_editora;`,
+                           FROM emprestimos, 
+                                        associados, 
+                                        livros, 
+                                        editoras
+                         WHERE emprestimos.id_livro = livros.id_livro AND
+                                        emprestimos.id_editora = editoras.id_editora AND
+                                        emprestimos.id_associado = associados.id_associado;`,
       (error, result, fields) => {
-        if (error) { return res.status(500).send({ errror: error }) }
+        if (error) {
+          return res.status(500).send({ errror: error });
+        }
         const response = {
           emprestimos: result.map(emprestimos => {
             return {
@@ -29,18 +34,22 @@ exports.getEmprestimos =  (req, res, next) => {
               'quantidade de emprestimos': result.length,
               detalhes: {
                 id_associado: emprestimos.id_associado,
-                associado: emprestimos.nome,
-                livro: emprestimos.id_livro,
-                editora: emprestimos.id_editora,
-                'nome editora': emprestimos.nome,
+                'nome': emprestimos.nome,
+                'sobrenome': emprestimos.sobrenome,
+                id_livro: emprestimos.id_livro,
+                titulo: emprestimos.titulo,
+                id_editora: emprestimos.id_editora,
+                'nome editora': emprestimos.nome_editora,
+                'data emprestimo': emprestimos.data_emprestimo,
                 'prazo devolucao': emprestimos.prazo_devolucao,
-                data: emprestimos.data,
-                'data devolucao': emprestimos.data_devolucao,
+                'data devolucao': emprestimos.data_devolucao
               },
               request: {
                 tipo: 'GET',
                 descricao: 'Retorrna os detalhes do empréstimo',
-                url: 'http://localhost:3000/emprestimos/' + emprestimos.id_emprestimo
+                url:
+                  'http://localhost:3000/emprestimos/' +
+                  emprestimos.id_emprestimo,
               },
             };
           }),
@@ -56,37 +65,37 @@ exports.postEmprestimos = (req, res, next) => {
     if (error) {
       return res.status(500).send({ error: error });
     }
-        conn.query(
-          'INSERT INTO emprestimos (id_associado, data, prazo_devolucao, data_devolucao) VALUES (?,?,?,?) ;',
-          [
-            req.body.id_associado,
-            req.body.data,
-            req.body.prazo_devolucao,
-            req.body.data_devolucao,
-          ],
-          (error, result, field) => {
-            conn.release();
-            if (error) {
-              return res.status(500).send({ error: error });
-            }
-            const response = {
-              mensagem: 'Empréstimo inserido.',
-              emprestimoCriado: {
-                id_emprestimo: result.id_emprestimo,
-                id_associado: req.body.id_associado,
-                data: req.body.data,
-                prazo_devolucao: req.body.prazo_devolucao,
-                data_devolucao: req.body.data_devolucao,
-                request: {
-                  tipo: 'GET',
-                  descricao: 'Retorna todos os emprestimos',
-                  url: 'http://localhost:3000/emprestimos',
-                },
-              },
-            };
-            return res.status(201).send(response);
-          }
-        );
+    conn.query(
+      'INSERT INTO emprestimos (id_associado, data, prazo_devolucao, data_devolucao) VALUES (?,?,?,?) ;',
+      [
+        req.body.id_associado,
+        req.body.data,
+        req.body.prazo_devolucao,
+        req.body.data_devolucao,
+      ],
+      (error, result, field) => {
+        conn.release();
+        if (error) {
+          return res.status(500).send({ error: error });
+        }
+        const response = {
+          mensagem: 'Empréstimo inserido.',
+          emprestimoCriado: {
+            id_emprestimo: result.id_emprestimo,
+            id_associado: req.body.id_associado,
+            data: req.body.data,
+            prazo_devolucao: req.body.prazo_devolucao,
+            data_devolucao: req.body.data_devolucao,
+            request: {
+              tipo: 'GET',
+              descricao: 'Retorna todos os emprestimos',
+              url: 'http://localhost:3000/emprestimos',
+            },
+          },
+        };
+        return res.status(201).send(response);
+      }
+    );
   });
 };
 
@@ -159,4 +168,3 @@ exports.deleteEmprestimo = (req, res, next) => {
     );
   });
 };
-
